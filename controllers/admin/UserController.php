25c3b37d3fc84a1f96ddd9ac7614a1c31a4e6c2a
Dan Zasiadko <zdaniil92@gmail.com>
2017-11-26 23:01:35 +0200

    my commit for yii2 project/controllers

---
+++ controllers/admin/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zdani
+ * Date: 26.11.2017
+ * Time: 22:51
+ */
+
+namespace app\controllers\admin;
+
+
+class UserController
+{
+
+}
---
+++ views/admin/user/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zdani
+ * Date: 26.11.2017
+ * Time: 22:56
+ */
